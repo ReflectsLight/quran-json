2c521b6d706fdada99510deaa0415bfa4e49bdb5
0x1eef <0x1eef@protonmail.com>
2022-07-16 16:19:50 -0300

    rubocop-ify

--- binlib/io/line.rb
+++ binlib/io/line.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class IO::Line
   require "io/console"
   attr_reader :io
--- gem.deps.rb
+++ gem.deps.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 gem "nokogiri", "~> 1.13"
 gem "paint", "~> 2.2"
 gem "json", "~> 2.6"
