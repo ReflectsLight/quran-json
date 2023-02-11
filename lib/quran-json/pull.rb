1a06953671da0d4018acd324d3c961ee8e1b6cea
0x1eef <0x1eef@protonmail.com>
2023-02-11 16:04:03 -0300

    Add French

--- lib/quran-json/pull.rb
+++ lib/quran-json/pull.rb
@@ -18,8 +18,8 @@ class Pull
     OptionParser.new(nil, 26, " " * 2) do |o|
       o.banner = "Usage: quran-json pull [OPTIONS]"
       op = o
-      o.on("-l", "--locale LOCALE", "ar, en, pt, fa, or nl (default: en)")
-      o.on("-o", "--overwrite", "Overwrite existing JSON files that might exist (default: no)")
+      o.on("-l", "--locale LOCALE", "ar, en, pt, fa, nl, or fr (default: en)")
+      o.on("-o", "--overwrite", "Overwrite existing JSON files (default: no)")
     end.parse(argv, into: options)
     options
   rescue
